ccec1b9a85bf068ac6750a01f453f08c5979a34c
Azat Ibrakov <azatibrakov@gmail.com>
2026-03-15 00:13:30 +0100

    Complete type annotations

--- unused/_core/missing.py
+++ unused/_core/missing.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
 import enum
-from typing import Final, Literal
+from typing import Final
 
 
 class Missing(enum.Enum):
     _VALUE = object()
 
 
-MISSING: Final[Literal[Missing._VALUE]] = Missing._VALUE  # noqa: SLF001
+MISSING: Final = Missing._VALUE  # noqa: SLF001
